3da652eb3e6f332ca4c903f7bc1640d187bec310
Emmanuel Ochoa <eorivera95@gmail.com>
2021-12-03 18:41:40 -0700

    Creating a Wrapper component for the navbar to display flex the Logo and the hamburger menu.

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
-  background-color: ${({ theme }) => theme.primaryBackgroundColor};
+  background-color: ${({ theme }) => theme.colors.background};
   padding: 2.3471em 0;
   margin: 0 auto;
 `;
 
 export const LogoContainer = styled.div`
   width: 5em;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;

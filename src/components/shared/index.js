6904f5fa58d15645d2d981507fee7dffb58d5ec3
Emmanuel Ochoa <eorivera95@gmail.com>
2021-12-04 12:08:24 -0700

    A reusable component to be used as a wrapper of a card.

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
+export const CardWrapper = styled.div`
+  border-radius: 8px;
+  border: 1px solid ${({ theme }) => theme.colors.palette.gray["900"]};
+  overflow: hidden;
+`;
+
 export const MainWrapper = styled.div`
   width: 90%;
   margin: 0 auto;

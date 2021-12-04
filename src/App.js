afe795ab301330b12f5fe2d921bb10c238fd878c
Emmanuel Ochoa <eorivera95@gmail.com>
2021-12-04 12:06:44 -0700

    Wrapping the page with a margin wrapper

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
 import { GlobalStyles } from "./global";
 import { theme } from "./theme";
+import { MainWrapper } from "./components/shared";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Header />
+      <MainWrapper>
+        <Dashboard />
+      </MainWrapper>
     </ThemeProvider>
   );
 };

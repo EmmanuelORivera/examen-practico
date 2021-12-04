8a9bf5717c307d296be6d61118f3b88239fa84da
Emmanuel Ochoa <eorivera95@gmail.com>
2021-12-04 12:06:12 -0700

    Refactoring the import from the MainWrapper component.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { ReactComponent as ZubutLogo } from "../../img/ZubutLogo.svg";
 import * as Styled from "./styles";
-import * as StyledShared from "../shared";
+import { MainWrapper } from "../shared";
 import HamburgerMenu from "./HamburgerMenu";
 import Navigation from "../Navigation";
 import Backdrop from "../Navigation/Backdrop";
@@ -13,14 +13,14 @@ const Header = () => {
       {open && <Navigation open={open} />}
       {open && <Backdrop setOpen={setOpen} />}
       <Styled.Container>
-        <StyledShared.MainWrapper>
+        <MainWrapper>
           <Styled.Wrapper>
             <Styled.LogoContainer>
               <ZubutLogo />
             </Styled.LogoContainer>
             <HamburgerMenu open={open} setOpen={setOpen} />
           </Styled.Wrapper>
-        </StyledShared.MainWrapper>
+        </MainWrapper>
       </Styled.Container>
     </>
   );

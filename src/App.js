135f3fec9ba6ff0f8efa796961c32ac04caa3e0f
Emmanuel Ochoa <eorivera95@gmail.com>
2021-12-05 16:55:05 -0700

    configuration of react-router-dom and adding the page About

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
 import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
 import { GlobalStyles } from "./global";
 import { theme } from "./theme";
 import { MainWrapper } from "./components/shared";
@@ -9,16 +11,21 @@ import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <div style={{ minHeight: "100vh" }}>
-        <Header />
-        <MainWrapper>
-          <Dashboard />
-        </MainWrapper>
-      </div>
-      <Footer />
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <div style={{ minHeight: "100vh" }}>
+          <Header />
+          <MainWrapper>
+            <Routes>
+              <Route path="/about" exact element={<About />} />
+              <Route path="/" exact element={<Dashboard />} />
+            </Routes>
+          </MainWrapper>
+        </div>
+        <Footer />
+      </ThemeProvider>
+    </Router>
   );
 };
 

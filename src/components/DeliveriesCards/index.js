67424efb9090b06f5534df0ed8d800a17b391bbb
Emmanuel Ochoa <eorivera95@gmail.com>
2021-12-05 12:29:52 -0700

    serviceInfo added to fill some date on the tracking card

--- src/components/DeliveriesCards/index.js
+++ src/components/DeliveriesCards/index.js
@@ -11,6 +11,12 @@ const initialState = {
       driver: "John Doe",
       shipper: "Liverpool",
       status: { text: "Entregado", code: "delivered" },
+      serviceInfo: {
+        estimatedArrival: "3 horas",
+        serviceStartedHour: "10:14 AM",
+        serviceFinishedHour: "1:14PM",
+        observations: "El paquete ha sido entregado con exito",
+      },
     },
     {
       id: "68c93f29",
@@ -19,6 +25,12 @@ const initialState = {
       driver: "Jane Doe",
       shipper: "Sigma",
       status: { text: "Cancelado", code: "canceled" },
+      serviceInfo: {
+        estimatedArrival: "1 hora",
+        serviceStartedHour: "10:14 AM",
+        serviceFinishedHour: "10:15 AM",
+        observations: "El envio ha sido cancelado por el usuario",
+      },
     },
     {
       id: "711b324d",
@@ -27,6 +39,12 @@ const initialState = {
       driver: "Bill Doe",
       shipper: "dportenis",
       status: { text: "En Progreso", code: "inProgress" },
+      serviceInfo: {
+        estimatedArrival: "2 horas",
+        serviceStartedHour: "10:14 AM",
+        serviceFinishedHour: "No ha finalizado",
+        observations: "Sin observaciones",
+      },
     },
   ],
   activeLink: null,

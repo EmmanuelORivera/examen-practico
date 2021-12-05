ef4348501995d7b25bd7372e2c6e27edb8ff6976
Emmanuel Ochoa <eorivera95@gmail.com>
2021-12-05 11:29:39 -0700

    share the same styles with the tracking and delivery cards

--- src/pages/Dashboard/shared/index.js
+++ src/pages/Dashboard/shared/index.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
+export const TemplateCard = styled.div`
+  min-width: 25.4em;
+  padding: 1em 0;
+  & p {
+    margin-bottom: 0.5em;
+  }
+`;
+
 export const CardsWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;

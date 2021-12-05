d25ad8d539b19f96f89e1ba9473395764e759e26
Emmanuel Ochoa <eorivera95@gmail.com>
2021-12-05 16:53:44 -0700

    closing navigation when a link is clicked

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,17 +4,24 @@ import { ReactComponent as AboutUs } from "../../img/AboutUs.svg";
 import ReactDOM from "react-dom";
 import * as Styled from "./styles";
 
-const Navigation = ({ open }) => {
+const Navigation = ({ open, setOpen }) => {
+  const handleClick = (e) => {
+    setOpen((prevOpen) => !prevOpen);
+  };
   const content = (
     <Styled.Container open={open}>
       <Styled.UnorderedList>
         <Styled.ListItem>
           <Dashboard />
-          <Styled.Link href="/#">Dashboard</Styled.Link>
+          <Styled.NewLink to="/" onClick={handleClick}>
+            Dashboard
+          </Styled.NewLink>
         </Styled.ListItem>
         <Styled.ListItem>
           <AboutUs />
-          <Styled.Link href="/#">About Us</Styled.Link>
+          <Styled.NewLink to="/about" onClick={handleClick}>
+            About Us
+          </Styled.NewLink>
         </Styled.ListItem>
       </Styled.UnorderedList>
     </Styled.Container>
